b61396262fae9dc9170e7c43ce3532c778c266b3
Eddie Knight <iv.eddieknight@gmail.com>
2018-03-07 18:33:04 -0600

    Typo fix

--- starfinder/starfinder_app.py
+++ starfinder/starfinder_app.py
@@ -6,7 +6,6 @@ CONF = config.CONF
 LOG = logging.get_logger(__name__)
 
 def create_app():
-    template_path = CONF.get('flask.templates.folder', default="templates")
-    LOG.debug("TEST:" template_path)
+    LOG.debug("TEST")
 
-
+create_app()
