92fecb5b193f7d340e915407696e11dd6632ae45
Eddie Knight <iv.eddieknight@gmail.com>
2018-03-20 18:59:02 -0500

    Added logic for create_database

--- starfinder/db/migrations/cli.py
+++ starfinder/db/migrations/cli.py
@@ -7,18 +7,30 @@ from alembic import command as alembic_command
 from alembic import config as alembic_config
 from alembic import util as alembic_util
 import click
+import sqlalchemy_utils
 
+from starfinder import config, logging
 from starfinder.db import models
 
-HEAD_FILENAME = "head"
+CONF = config.CONF
+LOG = logging.get_logger(__name__)
 
+HEAD_FILENAME = "head"
 ALEMBIC_INI = "alembic.ini"
 SCRIPT_LOCATION = "starfinder.db.migrations:alembic"
 MIGRATION_LOCATION = "starfinder.db.migrations:alembic_migrations"
+ENGINE_URL = CONF.get("DB_ENGINE_URL")
 
 @click.group()
 def migrate_cli():
     pass
+
+@migrate_cli.command(help="Creates the database. Must be run before any "
+                          "migrations as 'upgrade' itself will not create "
+                          "the database")
+def create_database():
+    if not sqlalchemy_utils.database_exists(ENGINE_URL):
+        sqlalchemy_utils.create_database(ENGINE_URL)
 
 
 def test_connection():
@@ -26,6 +38,7 @@ def test_connection():
         print("Couldn't connect to the database. Your engine URL is:")
         print(models.engine.url)
         sys.exit(1)
+    print("Connected Successfully.")
 
 
 def _dispatch_alembic_cmd(config, cmd, *args, **kwargs):
@@ -33,6 +46,14 @@ def _dispatch_alembic_cmd(config, cmd, *args, **kwargs):
         getattr(alembic_command, cmd)(config, *args, **kwargs)
     except alembic_util.CommandError as e:
         alembic_util.err(e)
+
+
+def _get_head_path(script):
+    if len(script.get_heads()) > 1:
+        alembic_util.err('Timeline branches unable to generate timeline')
+
+    head_path = os.path.join(script.versions, HEAD_FILENAME)
+    return head_path
 
 
 def _update_head_file(config):
@@ -48,7 +69,7 @@ def revision(ctx, message):
     test_connection()
     config = ctx.obj["alembic_config"]
     _dispatch_alembic_cmd(config, "revision", message=message)
-    _update_head_file(config)
+    # _update_head_file(config)
 
 
 def main():
@@ -59,5 +80,5 @@ def main():
                            SCRIPT_LOCATION)
 
     config.set_main_option("sqlalchemy.url",
-                           models.ENGINE_URL)
+                           ENGINE_URL)
     migrate_cli(obj={"alembic_config": config})
