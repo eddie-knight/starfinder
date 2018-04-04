b625763994a9f63d5aa6c4fbf87445ac9a81a6a5
CrossBeaux <bcbeckendorf@gmail.com>
2018-04-03 20:12:33 -0500

    Added 'show' route

--- starfinder/app/feats.py
+++ starfinder/app/feats.py
@@ -3,3 +3,7 @@ import flask
 feats = flask.Blueprint('feats', __name__, template_folder='templates')
 URL_PREFIX = '/feats'
 BLUEPRINT = feats
+
+@feats.route('/')
+def view_all():
+	return flask.render_template('feats/show.html')
