a455f04b4322d7e66260f54c9a6c72839c4ad13e
Eddie Knight <iv.eddieknight@gmail.com>
2018-04-13 16:51:24 -0500

    started generic update() route

--- starfinder/app/characters.py
+++ starfinder/app/characters.py
@@ -4,10 +4,12 @@ from flask import url_for, render_template, request, redirect
 
 from starfinder import forms, config, logging
 from starfinder.db import models
+from starfinder.helpers import helper
 
 CONF = config.CONF
 LOG = logging.get_logger(__name__)
 
+helper = helper.Helper()
 characters = flask.Blueprint('characters', __name__, template_folder='templates')
 URL_PREFIX = '/characters'
 BLUEPRINT = characters
@@ -31,6 +33,14 @@ def create():
 	models.Session.add(char)
 	models.Session.commit()
 	return redirect(url_for('characters.race_selection', char_id=char.id))
+
+
+@characters.route('/update_character', methods=['POST'])
+def update():
+	form = forms.CharacterRaceForm(request.form)
+	character = models.Character.get(form.id.data)
+	helper.update_character(form, character)
+	return redirect(url_for('characters.view_all', char_id=char.id))
 
 
 @characters.route('/race_selection/<uuid:char_id>', methods=['GET', 'POST'])
